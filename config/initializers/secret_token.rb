3c5dbc6aee4b21ad5cb137fa9aa243596d5321ba
Ivan Valdes R <monovaldes@gmail.com>
2013-04-06 08:33:20 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hdp::Application.config.secret_token = '52fed7b71f1b8ac9e445f341078d56a1f6854b8be127b401e262053acc348f80842170ce91b7c7f1e6ea35616005d00c25b1bdbe48fec2620da076b834c90cf3'
